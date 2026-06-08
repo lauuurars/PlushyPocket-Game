7836bf6087ba8c3b0fdd060181c86d95b2c587e4
lauurars <1120098177@u.icesi.edu.co>
2026-06-08 02:55:45 -0500

    fix: fix styles and function btn

--- frontend/src/phone/pages/WaitingRoom.tsx
+++ frontend/src/phone/pages/WaitingRoom.tsx
@@ -6,12 +6,12 @@ import FlorAzul from "../../assets/join/FlorAzul.svg";
 import Estrella4 from "../../assets/join/Estrella4.svg";
 import RayoRosa from "../../assets/join/RayoRosa.svg";
 import AngryMisu from "../../assets/join/AngryMisu.svg";
-import Corazon from "../../assets/welcome/Corazon.svg"
+import Corazon from "../../assets/welcome/Corazon.svg";
 import { PinkButton } from "../../components/PinkButton";
 import { createRealtimeSocket, fetchPartyRoomUserProfile } from "../../lib/api";
 import type { GameStartPayload } from "../../lib/api";
 import type { Socket } from "socket.io-client";
-import { getRoomState, attachRoomListeners, updateRoomState } from "../../lib/roomStore";
+import { getRoomState, attachRoomListeners, updateRoomState, resetRoomState } from "../../lib/roomStore";
 
 const GAME_ROUTES: Record<string, string> = {
     "cake": "/shout-cake",
@@ -74,6 +74,16 @@ export default function WaitingRoom() {
             socket.off("room_not_found");
         };
     }, [navigate, roomId, minigameId]);
+
+    const handleExitParty = () => {
+        const { socket } = getRoomState();
+        if (socket) {
+            socket.emit("player__leave", { roomId });
+            socket.disconnect();
+        }
+        resetRoomState();
+        navigate("/home-phone", { replace: true });
+    };
 
     return (
         <div
@@ -146,88 +156,72 @@ export default function WaitingRoom() {
                 />
             </div>
 
-            <div className="relative flex flex-col w-full" style={{ zIndex: 3, flex: 1 }}>
-                <h1
-                    className="m-0 text-center"
-                    style={{
-                        position: "absolute",
-                        top: "15%",
-                        left: "50%",
-                        transform: "translateX(-50%)",
-                        width: "min(295px, 84vw)",
-                        fontFamily: "'Baloo 2', cursive",
-                        fontWeight: 800,
-                        fontSize: "40px",
-                        lineHeight: "37px",
-                        letterSpacing: "-1px",
-                        color: "#FAFAFA",
-                        textShadow: "0 3px 16px rgba(0,0,0,0.12)",
-                    }}
-                >
-                    Waiting for
-                    <br />
-                    another player...
-                </h1>
-
-                <div
-                    className="absolute left-1/2"
-                    style={{
-                        top: "32%",
-                        transform: "translateX(-50%)",
-                        width: "min(190px, 52vw)",
-                    }}
-                >
-                    <div className="relative w-full">
-                        <img src={StarRoom} alt={`Sala ${roomId}`} className="block w-full" draggable={false} />
-                        <span
-                            style={{
-                                position: "absolute",
-                                inset: 0,
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                                paddingTop: "20px",
-                                fontFamily: "'Baloo 2', cursive",
-                                fontWeight: 800,
-                                fontSize: "47px",
-                                lineHeight: "96px",
-                                letterSpacing: "-1.34px",
-                                color: "#583921",
-                            }}
-                        >
-                            {roomId}
-                        </span>
-                    </div>
+            <div className="relative z-10 w-full flex-1 flex flex-col items-center justify-between pt-10 pb-0">
+                {/* Header (Title) */}
+                <div className="text-center pt-6">
+                    <h1
+                        className="m-0"
+                        style={{
+                            fontFamily: "'Baloo 2', cursive",
+                            fontWeight: 800,
+                            fontSize: "40px",
+                            lineHeight: "37px",
+                            letterSpacing: "-1px",
+                            color: "#FAFAFA",
+                            textShadow: "0 3px 16px rgba(0,0,0,0.12)",
+                        }}
+                    >
+                        Waiting for
+                        <br />
+                        another player...
+                    </h1>
                 </div>
 
-                <div
-                    className="absolute left-1/2 flex justify-center"
-                    style={{
-                        top: "63%",
-                        transform: "translateX(-50%)",
-                    }}
-                >
+                {/* Middle section (Star code and Exit Party Button) */}
+                <div className="flex-1 flex flex-col justify-center items-center gap-10 w-full z-20 min-h-0 py-6">
+                    {/* Star code */}
+                    <div className="w-[min(190px,52vw)]">
+                        <div className="relative w-full">
+                            <img src={StarRoom} alt={`Sala ${roomId}`} className="block w-full" draggable={false} />
+                            <span
+                                style={{
+                                    position: "absolute",
+                                    inset: 0,
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    paddingTop: "20px",
+                                    fontFamily: "'Baloo 2', cursive",
+                                    fontWeight: 800,
+                                    fontSize: "47px",
+                                    lineHeight: "96px",
+                                    letterSpacing: "-1.34px",
+                                    color: "#583921",
+                                }}
+                            >
+                                {roomId}
+                            </span>
+                        </div>
+                    </div>
+
+                    {/* Exit Party Button */}
                     <div style={{ transform: "scale(0.9)", transformOrigin: "center" }}>
                         <PinkButton
-                            text="Exit P|arty"
-                            onClick={() => {
-                                window.history.pushState(null, "", "/home-phone");
-                            }}
+                            text="Exit Party"
+                            onClick={handleExitParty}
                         />
                     </div>
                 </div>
-            </div>
 
-            <div
-                className="pointer-events-none absolute inset-x-0 bottom-1 z-20 flex justify-center"
-                aria-hidden
-            >
-                <img
-                    src={AngryMisu}
-                    alt=""
-                    className="w-[min(300px,86vw)] translate-y-1.5 object-contain object-bottom"
-                    draggable={false}
-                />
+                {/* Angry Misu - Anchored to bottom, in-flow to prevent overlap on short screens */}
+                <div className="w-[min(300px,86vw)] select-none pointer-events-none mt-auto z-10">
+                    <img
+                        src={AngryMisu}
+                        alt="Angry Misu"
+                        className="w-full h-auto block translate-y-1.5"
+                        draggable={false}
+                    />
+                </div>
             </div>
         </div>
     );
